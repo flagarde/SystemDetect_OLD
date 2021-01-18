40cbc3dce9f2dd872512b67344652048a5c5973a
flagarde <lagarde@sjtu.edu.cn>
2021-01-19 06:39:38 +0800

    Comment some macros

--- tests/SystemDetectMacro.cpp
+++ tests/SystemDetectMacro.cpp
@@ -25,11 +25,11 @@ int main() {
   std::cout << "Bingo3" << std::endl;
 #endif
 
-#if IS_BIG_ENDIAN()
+#if IS_BIG_ENDIAN
   std::cout << "Is big endian" << std::endl;
 #endif
 
-#if IS_LITTLE_ENDIAN()
+#if IS_LITTLE_ENDIAN
   std::cout << "Is little endian" << std::endl;
 #endif
 
