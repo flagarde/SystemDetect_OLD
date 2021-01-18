9c7e703d7fd99a5910f517b126f8ccbad2d1a937
Clang Robot <robot@example.com>
2021-01-18 18:51:21 +0000

    Committing clang-format changes

--- include/SystemDetect.hpp
+++ include/SystemDetect.hpp
@@ -1,25 +1,41 @@
 #pragma once
 
-namespace SystemDetect
-{
-  static const char* getArchitecture(){ return "@ARCH@"; }
-  static const char* getBaseArchitecture(){ return "@BASEARCH@"; }
-  static const char* getSystem(){ return "@CMAKE_SYSTEM_NAME@"; }
-  static const char* getCCompiler(){ return "@CMAKE_C_COMPILER_ID@"; }
-  static const char* getCXXCompiler(){ return "@CMAKE_CXX_COMPILER_ID@"; }
-  static const char* getCCompilerVersion(){ return "@CMAKE_C_COMPILER_VERSION@"; }
-  static const char* getCXXCompilerVersion(){ return "@CMAKE_CXX_COMPILER_VERSION@"; }
-  static const int getCCompilerMajorVersion(){ return @C_COMPILER_VERSION_MAJOR@; }
-  static const int getCCompilerMinorVersion(){ return @C_COMPILER_VERSION_MINOR@; }
-  static const int getCCompilerPatchVersion(){ return @C_COMPILER_VERSION_PATCH@; }
-  static const int getCXXCompilerMajorVersion(){ return @CXX_COMPILER_VERSION_MAJOR@; }
-  static const int getCXXCompilerMinorVersion(){ return @CXX_COMPILER_VERSION_MINOR@; }
-  static const int getCXXCompilerPatchVersion(){ return @CXX_COMPILER_VERSION_PATCH@; }
-  static const bool isBigEndian(){ return @ENDIANNESS@; }
-  static const bool isLittleEndian() {return !@ENDIANNESS@; }
-  static const char* getEndianness()
-  {
-      if(isBigEndian()) return "BigEndian";
-      else return "LittleEndian";
-  }
+namespace SystemDetect {
+static const char *getArchitecture() { return "@ARCH@"; }
+static const char *getBaseArchitecture() { return "@BASEARCH@"; }
+static const char *getSystem() { return "@CMAKE_SYSTEM_NAME@"; }
+static const char *getCCompiler() { return "@CMAKE_C_COMPILER_ID@"; }
+static const char *getCXXCompiler() { return "@CMAKE_CXX_COMPILER_ID@"; }
+static const char *getCCompilerVersion() {
+  return "@CMAKE_C_COMPILER_VERSION@";
 }
+static const char *getCXXCompilerVersion() {
+  return "@CMAKE_CXX_COMPILER_VERSION@";
+}
+static const int getCCompilerMajorVersion() {
+  return @C_COMPILER_VERSION_MAJOR @;
+}
+static const int getCCompilerMinorVersion() {
+  return @C_COMPILER_VERSION_MINOR @;
+}
+static const int getCCompilerPatchVersion() {
+  return @C_COMPILER_VERSION_PATCH @;
+}
+static const int getCXXCompilerMajorVersion() {
+  return @CXX_COMPILER_VERSION_MAJOR @;
+}
+static const int getCXXCompilerMinorVersion() {
+  return @CXX_COMPILER_VERSION_MINOR @;
+}
+static const int getCXXCompilerPatchVersion() {
+  return @CXX_COMPILER_VERSION_PATCH @;
+}
+static const bool isBigEndian() { return @ENDIANNESS @; }
+static const bool isLittleEndian() { return !@ENDIANNESS @; }
+static const char *getEndianness() {
+  if (isBigEndian())
+    return "BigEndian";
+  else
+    return "LittleEndian";
+}
+} // namespace SystemDetect
--- tests/SystemDetectMacro.cpp
+++ tests/SystemDetectMacro.cpp
@@ -2,37 +2,36 @@
 
 #include <iostream>
 
-int main()
-{
+int main() {
 #if ARCHITECTURE_IS(x86_64)
-  std::cout<<"x86_64"<<std::endl;
+  std::cout << "x86_64" << std::endl;
 #else
-  std::cout<<"not x86_64"<<std::endl;
+  std::cout << "not x86_64" << std::endl;
 #endif
-  
+
 #if BASE_ARCHITECTURE_IS(x86)
-  std::cout<<"OUI"<<std::endl;
+  std::cout << "OUI" << std::endl;
 #endif
-  
-#if CXX_COMPILER_MAJOR_MINOR_VERSION(10,2)
-  std::cout<<"Bingo"<<std::endl;
+
+#if CXX_COMPILER_MAJOR_MINOR_VERSION(10, 2)
+  std::cout << "Bingo" << std::endl;
 #endif
-  
-#if CXX_COMPILER_MAJOR_MINOR_VERSION(10,3)
-  std::cout<<"Bingo2"<<std::endl;
+
+#if CXX_COMPILER_MAJOR_MINOR_VERSION(10, 3)
+  std::cout << "Bingo2" << std::endl;
 #endif
-  
-#if CXX_COMPILER_MAJOR_MINOR_VERSION(9,2)
-  std::cout<<"Bingo3"<<std::endl;
+
+#if CXX_COMPILER_MAJOR_MINOR_VERSION(9, 2)
+  std::cout << "Bingo3" << std::endl;
 #endif
-  
+
 #if IS_BIG_ENDIAN()
-  std::cout<<"Is big endian"<<std::endl;
+  std::cout << "Is big endian" << std::endl;
 #endif
-  
+
 #if IS_LITTLE_ENDIAN()
-  std::cout<<"Is little endian"<<std::endl;
+  std::cout << "Is little endian" << std::endl;
 #endif
-  
+
   return 0;
 }
