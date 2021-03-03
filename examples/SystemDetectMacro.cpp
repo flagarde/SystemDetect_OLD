f70ac70fd41fe3516e921b3abe5285c73f0bc427
flagarde <lagarde@sjtu.edu.cn>
2021-03-03 21:58:54 +0800

    Cleaning

--- examples/SystemDetectMacro.cpp
+++ examples/SystemDetectMacro.cpp
@@ -9,20 +9,8 @@ int main() {
   std::cout << "not x86_64" << std::endl;
 #endif
 
-#if BASE_ARCHITECTURE_IS(x86)
-  std::cout << "OUI" << std::endl;
-#endif
-
 #if CXX_COMPILER_MAJOR_VERSION(10) && CXX_COMPILER_MINOR_VERSION(2)
   std::cout << "Bingo" << std::endl;
-#endif
-
-#if CXX_COMPILER_MAJOR_VERSION(10) && CXX_COMPILER_MINOR_VERSION(3)
-  std::cout << "Bingo2" << std::endl;
-#endif
-
-#if CXX_COMPILER_MAJOR_VERSION(9) && CXX_COMPILER_MINOR_VERSION(2)
-  std::cout << "Bingo3" << std::endl;
 #endif
 
 #if IS_BIG_ENDIAN
