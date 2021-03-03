4d2dc225adf37b7fcac10c9916d9f5427df6817f
flagarde <lagarde@sjtu.edu.cn>
2021-03-03 23:27:40 +0800

    Fix

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -8,19 +8,19 @@ int main() {
 #if ARCHITECTURE_IS(TEST_ARCH)
   std::cout << "I guessed the right architecture : "+std::string(TEST_ARCH) << std::endl;
 #else
-  #error "Wrong Architecture"
+  #error Wrong Architecture
 #endif
 
 #if SYSTEM_IS(TEST_SYSTEM)
   std::cout << "I guessed the right system : "+std::string(TEST_ARCH) << std::endl;
 #else
-  #error "Wrong System"
+  #error Wrong System
 #endif
 
 #if C_COMPILER_IS(TEST_COMPILER)
   std::cout << "I guessed the right C compiler : "+std::string(TEST_COMPILER) << std::endl;
 #else
-  #error "Wrong Compiler"
+  #error Wrong Compiler
 #endif
 
   return 0;
