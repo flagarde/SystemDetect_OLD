b8e48f0c4a971ca6048ce3d3906a2f74b919b883
flagarde <lagarde@sjtu.edu.cn>
2021-03-04 19:16:03 +0800

    Fix

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -5,6 +5,23 @@
 
 int main() {
 
+std::cout << "System : " << SystemDetect::getSystem() << std::endl;
+std::cout << "Architecture : " << SystemDetect::getArchitecture() << std::endl;
+std::cout << "Base architecture : " << SystemDetect::getBaseArchitecture() << std::endl;
+std::cout << "C compiler : " << SystemDetect::getCCompiler() << std::endl;
+std::cout << "C compiler version : " << SystemDetect::getCCompilerVersion() << std::endl;
+std::cout << "C compiler major : " << SystemDetect::getCCompilerMajorVersion() << std::endl;
+std::cout << "C compiler minor : " << SystemDetect::getCCompilerMinorVersion() << std::endl;
+std::cout << "C compiler patch : " << SystemDetect::getCCompilerPatchVersion() << std::endl;
+std::cout << "CXX compiler : " << SystemDetect::getCXXCompiler() << std::endl;
+std::cout << "CXX compiler version : " << SystemDetect::getCXXCompilerVersion() << std::endl;
+std::cout << "CXX compiler major : " << SystemDetect::getCXXCompilerMajorVersion() << std::endl;
+std::cout << "CXX compiler minor : " << SystemDetect::getCXXCompilerMinorVersion() << std::endl;
+std::cout << "CXX compiler patch : " << SystemDetect::getCXXCompilerPatchVersion() << std::endl;
+std::cout << "Endianness : " << SystemDetect::getEndianness() << std::endl;
+std::cout << "Is bigendian : " << SystemDetect::isBigEndian() << std::endl;
+std::cout << "Is littleendian : " << SystemDetect::isLittleEndian()<< std::endl;
+
 #if defined(Clang)
   #if C_COMPILER_IS(Clang)
   std::cout << "I guessed the right architecture : Clang" << std::endl;
@@ -44,7 +61,6 @@ int main() {
     #error Wrong system
   #endif
 #endif
-
 
 #if defined(x86_64)
   #if ARCHITECTURE_IS(x86_64)
